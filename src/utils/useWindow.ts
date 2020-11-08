18a29bd1df2857132011c9f51ae185f9e7044e52
Nuttapong Saelek <Nuttapong_s@kkumail.com>
2020-11-08 10:00:14 +0700

    added useCalwidth

---
+++ src/utils/useWindow.ts
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+export const useViewport = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return width;
+};
+
+export const useCalWidth = (
+  initWidth: number,
+  initWidthPercent: number,
+  scaleVal?: number,
+  base: number = 2
+) => {
+  const currentWidth = useViewport();
+  const normalizedBy = (100 - initWidthPercent) / 100;
+  const scale = scaleVal || initWidth * normalizedBy;
+  const expoVal = (initWidth - currentWidth) / scale;
+
+  const GeoSequence = (expoVal: number) => {
+    return Math.pow(base, expoVal - 1);
+  };
+
+  if (expoVal < 0) {
+    return Math.min(initWidthPercent + GeoSequence(Math.abs(expoVal)), 100);
+  } else {
+    return initWidthPercent - GeoSequence(expoVal);
+  }
+};
