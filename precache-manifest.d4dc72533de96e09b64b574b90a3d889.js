4dbb7beecbb5c651ee61e97428d49153d60f2333
Nuttapong Saelek <Nuttapong_s@kkumail.com>
2020-11-14 19:34:03 +0700

    Updates

---
+++ precache-manifest.d4dc72533de96e09b64b574b90a3d889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178b20b6ce4c6f3c73019e6e2da81e88",
+    "url": "/San-Kanban/index.html"
+  },
+  {
+    "revision": "1e0485e6827cf0d238a2",
+    "url": "/San-Kanban/static/css/main.05d87e03.chunk.css"
+  },
+  {
+    "revision": "e62cd7014d1cfc7dfc42",
+    "url": "/San-Kanban/static/js/2.8206026e.chunk.js"
+  },
+  {
+    "revision": "a6e453fc829f927220f292620a8c0246",
+    "url": "/San-Kanban/static/js/2.8206026e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0485e6827cf0d238a2",
+    "url": "/San-Kanban/static/js/main.13bbdfbd.chunk.js"
+  },
+  {
+    "revision": "5f99b4f319063b654358",
+    "url": "/San-Kanban/static/js/runtime-main.97dbe5c8.js"
+  }
+]);
