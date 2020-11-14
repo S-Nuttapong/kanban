de180c2a7085c2af5b9948d737818bc2cb397183
Nuttapong Saelek <Nuttapong_s@kkumail.com>
2020-11-14 20:39:08 +0700

    changed public, fixed css styled

--- src/components/board/styles.ts
+++ src/components/board/styles.ts
@@ -5,12 +5,13 @@ export const BoardContainer = styled(CustomDragContainer)`
   background-color: #ebecf0;
   width: 300px;
   min-height: 40px;
+  max-height: 90%;
   margin-right: 20px;
   border-radius: 3px;
   padding: 8px 8px;
   flex-shrink: 0;
   z-index: 10;
-  
+  overflow-y: scroll;
 
 `;
 
