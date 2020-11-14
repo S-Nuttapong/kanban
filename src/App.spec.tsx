0266ce4bed64f53dc1383a42e9349e5b60e653d3
Nuttapong Saelek <Nuttapong_s@kkumail.com>
2020-11-14 19:16:54 +0700

    removed test suite

--- src/App.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { App } from "./App";
-import { render } from "@testing-library/react";
-
-describe("App", () => {
-  describe("on 'Add New Board' click", () => {
-    it.todo("call passdd function");
-  });
-});
