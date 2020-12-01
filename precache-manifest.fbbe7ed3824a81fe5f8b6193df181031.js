7a1fe8b653d696caa65515a1721c5ace0a20c8fd
Nuttapong Saelek <Nuttapong_s@kkumail.com>
2020-12-01 19:16:41 +0700

    Updates

---
+++ precache-manifest.fbbe7ed3824a81fe5f8b6193df181031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "162aae0d9bfe68b1d8b63f1e63f16afe",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "b9c340f77c5c3d450af9",
+    "url": "/kanban/static/css/main.e1748df0.chunk.css"
+  },
+  {
+    "revision": "90d7a6a013db34f3acd1",
+    "url": "/kanban/static/js/2.8e85f4a1.chunk.js"
+  },
+  {
+    "revision": "a6e453fc829f927220f292620a8c0246",
+    "url": "/kanban/static/js/2.8e85f4a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c340f77c5c3d450af9",
+    "url": "/kanban/static/js/main.bf38a3a5.chunk.js"
+  },
+  {
+    "revision": "cc32e42deb13310fb084",
+    "url": "/kanban/static/js/runtime-main.c8a5115f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kanban/precache-manifest.9403b96448ba8d153e36baa68dcfb29f.js"
+  "/kanban/precache-manifest.fbbe7ed3824a81fe5f8b6193df181031.js"
 );
 
 self.addEventListener('message', (event) => {
